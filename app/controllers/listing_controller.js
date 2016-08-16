b469768907abe254665ddd5140928c6fed8a0072
Henry Wilson <henryhwilson@gmail.com>
2016-08-16 15:36:02 -0400

    listings linked to other tables

--- app/controllers/listing_controller.js
+++ app/controllers/listing_controller.js
@@ -20,29 +20,16 @@ export const createListing = (req, res) => {
 
 export const getListings = (req, res) => {
   Listing.find()
-  .populate('host_user_id', 'location_id')
   .sort('-start_time')
   .exec((error, listings) => {
     res.json(listings.map(listing => {
-      if (listing.host_user_id && listing.location_id) {
-        return {
-          listing_id: listing._id,
-          location_name: listing.location_id.location_name,
-          location_id: listing.location_id._id,
-          host_user_id: listing.host_user_id._id,
-          host_user_name: listing.host_user_id.full_name,
-          num_still_needed_for_game: GAME_SIZE - listing.num_looking_for_game - listing.users.length,
-          start_time: listing.start_time,
-        };
-      } else {
-        return {
-          listing_id: listing._id,
-          location_id: listing.location_id,
-          host_user_id: listing.host_user_id,
-          num_still_needed_for_game: GAME_SIZE - listing.num_looking_for_game - listing.users.length,
-          start_time: listing.start_time,
-        };
-      }
+      return {
+        listing_id: listing._id,
+        location_id: listing.location_id,
+        host_user_id: listing.host_user_id,
+        num_still_needed_for_game: GAME_SIZE - listing.num_looking_for_game - listing.users.length,
+        start_time: listing.start_time,
+      };
     }));
   });
 };
